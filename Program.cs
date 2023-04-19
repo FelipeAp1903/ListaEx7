ab1d79a3f4b52c44ab405b6724b8d33a7a2d854f
FelipeAp1903 <131301360+FelipeAp1903@users.noreply.github.com>
2023-04-19 17:14:33 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double n1;
+            Double quilometro=1.852;
+            Double soma;
+            Console.WriteLine("coloque o valor em milha marítimas:");
+            n1 = Double.Parse(Console.ReadLine());
+            soma = n1 * quilometro;
+            Console.WriteLine("O valor em quilometros é:{0}",soma);
+
+        }
+    }
+}
